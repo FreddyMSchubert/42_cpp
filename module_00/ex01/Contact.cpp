e6af3a28969b184d954eb7ae8c9447a6c9afed4e
FreddyMSchubert <github@frederickschubert.de>
2024-08-19 13:56:22 +0200

    00/01 - Contact data retrieval
    Contact data setting, phone number validation & printing

--- module_00/ex01/Contact.cpp
+++ module_00/ex01/Contact.cpp
@@ -1,20 +1,74 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*   contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fschuber <fschuber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: freddy <freddy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 11:28:02 by fschuber          #+#    #+#             */
-/*   Updated: 2024/08/19 11:57:53 by fschuber         ###   ########.fr       */
+/*   Updated: 2024/08/19 13:55:02 by freddy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
-Contact::Contact() : firstName(""), lastName(""), nickName(""), phoneNumber(""), secret(""), creationTimestamp(-1), hasData(false) {}
+Contact::Contact() : creationTimestamp(-1), hasData(false), firstName(""), lastName(""), nickName(""), phoneNumber(""), secret("") {}
 Contact::~Contact() {}
 
-void Contact::InitContact()
+/* ----- INPUT RETRIEVAL ----- */
+
+void	Contact::InitContact()
 {
+	GetInputLine("first name", &firstName);
+	GetInputLine("last name", &lastName);
+	GetInputLine("nickname", &nickName);
+	do
+	{
+		GetInputLine("phone number", &phoneNumber);
+		if (!IsValidPhoneNumber(phoneNumber))
+			std::cout << "Invalid phone number. Please enter a valid phone number." << std::endl;
+	}
+	while (!IsValidPhoneNumber(phoneNumber));
+	GetInputLine("darkest secret", &secret);
+	creationTimestamp = time(0);
+	hasData = true;
+
+	std::cout << "Contact " << firstName << " " << lastName << " has been created." << std::endl;
+	Contact::PrintContact();
 }
+
+void	Contact::GetInputLine(std::string prompt, std::string *dest)
+{
+	std::string input;
+
+	std::cout << "Enter " << prompt << ": ";
+	std::getline(std::cin, input);
+	while (input.empty())
+	{
+		if (std::cin.eof()) break;
+		std::cout << "Please enter a valid " << prompt << ": ";
+		std::getline(std::cin, input);
+	}
+	*dest = input;
+}
+
+bool	Contact::IsValidPhoneNumber(const std::string& phoneNumber)
+{
+	for (char c : phoneNumber)
+		if (!isdigit(c) && c != ' ' && c != '+')
+			return false;
+	return true;
+}
+
+/* ----- DATA DISPLAY ----- */
+
+void	Contact::PrintContact()
+{
+	std::cout << "Name:\t\t" << firstName << " \"" << nickName << "\" " << lastName << std::endl;
+	std::cout << "Phone number:\t" << phoneNumber << std::endl;
+	std::cout << "Secret:\t\t" << secret << std::endl;
+}
+
+// Table Formatting
+
+// ...
--- module_00/ex01/Contact.hpp
+++ module_00/ex01/Contact.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fschuber <fschuber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: freddy <freddy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 11:37:35 by fschuber          #+#    #+#             */
-/*   Updated: 2024/08/19 11:59:21 by fschuber         ###   ########.fr       */
+/*   Updated: 2024/08/19 13:51:09 by freddy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,19 @@
 
 #include <ctime>
 #include <string>
+#include <iostream>
 
 class Contact
 {
 	public:
+		Contact();
+		~Contact();
+
 		time_t		creationTimestamp;
 		bool		hasData;
 
 		void		InitContact();
+		void		PrintContact();
 
 	private:
 		std::string	firstName;
@@ -30,6 +35,9 @@ class Contact
 		std::string	nickName;
 		std::string	phoneNumber;
 		std::string	secret;
+
+		void		GetInputLine(std::string prompt, std::string *dest);
+		bool		IsValidPhoneNumber(const std::string& phoneNumber);
 };
 
 #endif
