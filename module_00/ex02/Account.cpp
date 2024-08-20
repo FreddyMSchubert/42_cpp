b00c0091c02a422170252c932a27d67a665d0c09
FreddyMSchubert <github@frederickschubert.de>
2024-08-20 03:36:05 +0200

    00/02 basic account.cpp setup

--- module_00/ex02/Account.cpp
+++ module_00/ex02/Account.cpp
@@ -3,10 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fschuber <fschuber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: freddy <freddy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 17:27:48 by fschuber          #+#    #+#             */
-/*   Updated: 2024/08/19 17:27:49 by fschuber         ###   ########.fr       */
+/*   Updated: 2024/08/20 03:35:17 by freddy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+
+Account::Account(int initial_deposit)
+{
+	// hm....
+}
+Account::~Account() {}
+
+int	Account::getNbAccounts(void)
+{ return (_nbAccounts); }
+int	Account::getTotalAmount(void)
+{ return (_totalAmount); }
+int	Account::getNbDeposits(void)
+{ return (_totalNbDeposits); } 
+int	Account::getNbWithdrawals(void)
+{ return (_totalNbWithdrawals); }
